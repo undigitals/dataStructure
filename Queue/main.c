6ca4eb003f4e27ba9d3069667f0504d5f2d377a2
Umid Negmatullayev <undigitals@gmail.com>
2018-04-04 16:05:36 +0900

    Initial Commit

---
+++ Queue/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Queue
+//
+//  Created by Umid Negmatullayev on 4/4/18.
+//  Copyright © 2018 Umid Negmatullayev. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
