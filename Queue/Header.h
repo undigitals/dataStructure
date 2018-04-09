9f53add8f13578c2cbc52de62bd5a98aaa628652
Umid Negmatullayev <undigitals@gmail.com>
2018-04-09 16:54:58 +0900

    "queue"

--- Queue/Header.h
+++ Queue/Header.h
@@ -1,32 +1,128 @@
-
-#ifndef Header_h
-#define Header_h
+/* ADT for Queue*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct node{
     void* dataPtr;
     struct node* next;
 }QUEUE_NODE;
 
-typedef struct{
+typedef struct {
     QUEUE_NODE* front;
     QUEUE_NODE* rear;
     int count;
 }QUEUE;
 
-// function prototypes
+// Prototype declarations
 QUEUE* createQueue (void);
 QUEUE* destroyQueue (QUEUE* queue);
 
 bool dequeue (QUEUE* queue, void** itemPtr);
 bool enqueue (QUEUE* queue, void* itemPtr);
-bool queueFront (QUEUE* queue, void** itemPtr);
-bool queueRear (QUEUE* queue, void** itemPtr);
 int queueCount (QUEUE* queue);
 
 bool emptyQueue (QUEUE* queue);
-bool fullQueue (QUEUE* queue);
-
-//End of Queue ADT Definitions
 
 
-#endif /* Header_h */
+void display(QUEUE* queue);
+// End of Queue ADT Definitions
+
+
+/* ===== createQueue =====*/
+
+QUEUE* createQueue(void){
+    // Local Definitions
+    QUEUE* queue;
+    
+    // Statements
+    queue = (QUEUE*) malloc (sizeof(QUEUE));
+    if(queue){
+        queue->front = NULL;
+        queue->rear = NULL;
+        queue->count = 0;
+    } // if
+    return queue;
+} // createQueue
+
+/*===== enqueue =====*/
+bool enqueue(QUEUE* queue, void* itemPtr){
+    // Local Definitions
+    QUEUE_NODE* newPtr;
+    
+    // Statements
+    if(!(newPtr = (QUEUE_NODE*) malloc (sizeof(QUEUE_NODE)))){
+        return false;
+    }
+    
+    newPtr->dataPtr = itemPtr;
+    newPtr->next = NULL;
+    
+    if(queue->count == 0){
+        // Inserting into null queue
+        queue->front = newPtr;
+    }else{
+        queue->rear->next = newPtr;
+    }
+    
+    (queue->count)++;
+    queue->rear = newPtr;
+    return true;
+} // enqueue
+
+
+/* ===== dequeue =====*/
+bool dequeue (QUEUE* queue, void** itemPtr){
+    // Local Definitions
+    QUEUE_NODE* deleteLoc;
+    
+    // Statements
+    if(!queue->count){
+        return false;
+    }
+    *itemPtr = queue->front->dataPtr;
+    deleteLoc = queue->front;
+    if(queue->count==1){
+        // Deleting only item in queue
+        queue->rear = queue->front = NULL;
+    }else{
+        queue->front= queue->front->next;
+    }
+    (queue->count)--;
+    free(deleteLoc);
+    
+    return true;
+} // dequeue
+
+
+/*===== emptyQueue =====*/
+bool emptyQueue(QUEUE* queue){
+    // Statements
+    return (queue->count==0);
+} // emptyQueue
+
+
+/*===== display =====*/
+void display(QUEUE* queue){
+    QUEUE_NODE* temp;
+    if(emptyQueue(queue)){
+        printf("Nothing to display!!");
+    }
+    temp=queue->front;
+    int sum =(int)temp->dataPtr;
+    while(temp!=NULL){
+        printf("%i \t", sum);
+        temp=temp->next;
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
--- Queue/main.c
+++ Queue/main.c
@@ -1,1 +1,36 @@
+/*===== createQueue =====*/
+#include "Header.h"
 
+int main(){
+    int choice,*item;
+    QUEUE* categ;
+    categ  = createQueue();
+    
+    while(1){
+        printf("\n 1.Enqueue: \n");
+        printf("\n 2.Dequeue: \n");
+        printf("3.Display: \n");
+        printf("\n 4.Exit: \n");
+        
+        scanf("%i",&choice);
+        
+        switch (choice) {
+            case 1:
+                scanf("%i", item);
+                enqueue(categ, item);
+                break;
+            case 2:
+                dequeue(categ, item);
+                break;
+            case 3:
+                display(categ);
+                break;
+            case 4:
+                exit(0);
+            default:
+                break;
+        }
+    }
+    
+    return 0;
+}
