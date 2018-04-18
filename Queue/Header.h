ebe1f3df419521bc72fdb70ce9a732d648c29b90
Umid Negmatullayev <undigitals@gmail.com>
2018-04-18 14:03:40 +0900

    "first trial"

--- Queue/Header.h
+++ Queue/Header.h
@@ -1,114 +1,54 @@
-//
-//  Header.h
-//  Queue
-//
-//  Created by Umid Negmatullayev on 4/13/18.
-//  Copyright © 2018 Umid Negmatullayev. All rights reserved.
-//
-#ifndef Header_h
-#define Header_h
+#ifndef Queue
+#define Queue
+
 #include <stdlib.h>
 #include <stdbool.h>
-#include <stdio.h>
 
-
-// declaring the QUEUE_NODE
-typedef struct NODE{
-    void* data;
-    struct NODE* next;
-}NODE;
-
-// declaring the QUEUE
-typedef struct {
-    NODE* front;
-    NODE* rear;
+// defining queue
+typedef struct QUEUE{
+    void* buffer[10];
+    int front;
+    int rear;
     int cnt;
 }QUEUE;
 
-// initializing the QUEUE
-QUEUE* creatingQueue(){
+QUEUE* createQueue(){
     QUEUE* queue;
-    queue = (QUEUE*)malloc(sizeof(QUEUE));
+    queue=(QUEUE*)malloc(sizeof(QUEUE));
     
     if(queue){
-        queue->front=NULL;
-        queue->rear = NULL;
+        queue->front=0;
+        queue->rear=0;
         queue->cnt=0;
     }
     return queue;
 }
 
-// function prototypes
-bool isEmpty(QUEUE*);
-bool dequeue(QUEUE* queue, void** dataPtr);
-bool enqueue(QUEUE* queue, void* data);
-bool isEmpty(QUEUE* queue);
+QUEUE destroy(QUEUE* queue){
+    
+}
 
-
-// enqueueu
 bool enqueue(QUEUE* queue, void* data){
-    NODE* node;
     
-    // allocating storage for node
-    node = (NODE*)malloc(sizeof(NODE));
-    
-    // checking if node created in the heap
-    if(!node){
+    if(queue->cnt==10){
         return false;
     }
+    queue->buffer[queue->rear]=data;
+    queue->rear=(queue->rear+1)%10;
     
-    // assigning usr data to node data
-    node->data=data;
-    node->next=NULL;
-    
-    // if queue is empty we assign both front and rear to the node
-    if(queue->cnt==0)
-        queue->front=node;
-    
-    // if queue is not empty we just assign rear->next to node
-    else
-        queue->rear->next=node;
-    
-    
-    // in any case we need to upgade count and we shift rear to next node
     (queue->cnt)++;
-    queue->rear=node;
     return true;
 }
 
-bool dequeue(QUEUE* queue, void** dataPtr){
-    NODE* temp=NULL;
-    
-    if(!isEmpty(queue)){
-        *dataPtr = queue->front->data;
-        temp = queue->front;
-    }else{
+bool dequeue(QUEUE* queue, void** data){
+    if(queue->cnt==0){
         return false;
     }
-    if(queue->cnt==1){
-        queue->rear=queue->front=NULL;
-    }else{
-        queue->front=queue->front->next;
-    }
+    *data=queue->buffer[queue->front];
+    queue->front=(queue->front+1)%10;
     
     (queue->cnt)--;
-    free(temp);
-    
     return true;
 }
 
-
-bool isEmpty(QUEUE* queue){
-    if(queue->cnt==0)
-        return true;
-    return false;
-}
-
-
-
-
-
-
-
-#endif /* Header_h */
-
+#endif
